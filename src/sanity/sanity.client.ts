aebca1a74f6d5ebe8c70fc6469338b4bcc8db9a2
Areeba-binteali <154247306+Areeba-binteali@users.noreply.github.com>
2025-02-01 17:47:24 +0500

    Delete src/sanity directory

--- src/sanity/sanity.client.ts
+++
@@ -1,9 +0,0 @@
-// import { createClient } from "next-sanity";
-
-// export const sanityClient = createClient({
-//     projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
-//     dataset : process.env.NEXT_PUBLIC_SANITY_DATASET || "",
-//     apiVersion : "2024-12-18",
-//     useCdn : false,
-//     token : process.env.SANITY_API_TOKEN || "",
-// })
--- src/sanity/schema/index.ts
+++
@@ -1,4 +0,0 @@
-import { Category } from "./category";
-import { product } from "./prdouct";
-
-export const schemaType = [product,Category]
